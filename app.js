83f26e54ff23854cbc45bcca374e527ae5f31878
night <yseddiq@gmail.com>
2022-08-14 23:01:08 -0400

    added inquirer package

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const inquirer= require('inquirer');
+/*
 const fs = require('fs');
 const generatePage= require('./src/page-template.js');
-const profileDataArgs= process.argv.slice(2);
-const [name, github]=profileDataArgs;
+
+const pageHTML= generatePage(name, github);
 
 fs.writeFile('index.html', generatePage(name,github), err=>{
     if (err) throw err;
     console.log('Portfolio Complete! checkout index.html to see the output!');
 });
+*/
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
