40a41640fad187c246cfebb236a139b8d8c55186
night <yseddiq@gmail.com>
2022-08-16 05:02:10 -0400

    finished HTML generation

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer= require('inquirer');
-/*
+
 const fs = require('fs');
 const generatePage= require('./src/page-template.js');
-
+/*
 const pageHTML= generatePage(name, github);
 
 fs.writeFile('index.html', generatePage(name,github), err=>{
@@ -143,5 +143,10 @@ const promptProject = portfolioData =>{
 promptUser()
     .then(promptProject)
     .then(portfolioData=>{
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err=>{
+            if (err) throw new Error(err);
+
+        });
     });
